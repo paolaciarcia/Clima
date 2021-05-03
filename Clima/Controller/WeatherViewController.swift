8e6867e9eb100412567823806d8b2feabae42393
= <=>
2021-05-03 09:50:01 -0300

    add project

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,53 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+    
     }
+  
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        if searchTextField.text != nil {
+            print(searchTextField.text!)
+        }
+        view.endEditing(true)
+    }
+}
 
+extension WeatherViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if searchTextField.text != nil {
+            print(searchTextField.text!)
+        }
+        view.endEditing(true)
+        
+        return true
+    }
+    //validate what the user wrote
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField.text to get the weather for that city
+        
+        if let city = searchTextField.text {
+        weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
 }
 
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -1,9 +1,54 @@
-//
-//  WeatherManager.swift
-//  Clima
-//
-//  Created by Erick Borges on 28/04/21.
-//  Copyright © 2021 App Brewery. All rights reserved.
-//
+
 
 import Foundation
+
+struct WeatherManager {
+    //Networking
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e87980a7ca46a859b7faad8734c24120&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        
+        //1.Create a URL
+        if let url = URL(string: urlString) {
+            //2.Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3.Give URLSession a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                
+                if error != nil {
+                    print("houve um erro: \(error!)")
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJason(weatherData: safeData)
+                    print("o objeto data é nulo")
+                }
+            }
+            //4.Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJason(weatherData: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+            let weather = decodedData.weather[0].description
+            
+            print(weather)
+        } catch { 
+            print(error)
+        }
+    }
+}
